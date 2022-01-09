e36ef80d20ae77b34ede5671e4f0743e1cd8d3ef
ThreeCucumbers <95982616+ThreeCucumbers@users.noreply.github.com>
2022-01-09 19:10:34 +0200

    Add files via upload

---
+++ howassignearrayworks.js
@@ -0,0 +1,21 @@
+
+let varA = "hohoho"
+let varB = varA
+
+console.log(varA)
+console.log(varB)
+
+varA = "hihihi"
+
+console.log(varB)
+
+//------------------------------------------------------
+let arrOne =[1,2,3] ;
+let arrTwo = arrOne ;
+
+console.log(arrOne) ;
+console.log(arrTwo) ;
+
+arrOne.push(00,1) ;
+
+console.log(arrTwo) ;
