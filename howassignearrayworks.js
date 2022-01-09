bfbc1d2f1be64b3ee411197ad037f500fd4bcafc
ThreeCucumbers <95982616+ThreeCucumbers@users.noreply.github.com>
2022-01-09 20:20:46 +0200

    Final file!

--- howassignearrayworks.js
+++ howassignearrayworks.js
@@ -1,4 +1,6 @@
 // Bro run this code ,its interesting to see whats going on!
+
+//ASSIGNING OF VARIABLES
 let varA = "hohoho"
 let varB = varA
 
@@ -9,7 +11,7 @@ varA = "hihihi"
 
 console.log(varB)
 
-//------------------------------------------------------
+//ASSIGNING OF ARRAYS 1
 let arrOne =[1,2,3] ;
 let arrTwo = arrOne ;
 
@@ -17,5 +19,15 @@ console.log(arrOne) ;
 console.log(arrTwo) ;
 
 arrOne.push(00,1) ;
-
 console.log(arrTwo) ;
+
+arrTwo.push(55) ;
+console.log(arrOne)
+
+console.log(arrOne === arrTwo)
+
+//ASSIGNING OF ARRAYS 2
+let arrThree =[1,2,3,55]
+let arrFive =[1,2,3,55]
+
+console.log(arrThree === arrFive)
